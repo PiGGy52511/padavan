c92fd4e08524352097a50287e4b4528a18e0d335
PiGGy52511 <32626479+PiGGy52511@users.noreply.github.com>
2023-09-09 08:27:59 +0800

    Update dnsmasq.sh

--- trunk/user/dnsmasq/dnsmasq.sh
+++ trunk/user/dnsmasq/dnsmasq.sh
@@ -2,13 +2,13 @@
 
 filter_aaaa=`nvram get dhcp_filter_aaa`
 min_ttl=`nvram get dhcp_min_ttl`
-if [ $filter_aaaa = 0 ]; then
-sed -i '/filter-aaaa/d' /etc/storage/dnsmasq/dnsmasq.conf
- else 
-sed -i '/filter-aaaa/d' /etc/storage/dnsmasq/dnsmasq.conf
-	cat >>/etc/storage/dnsmasq/dnsmasq.conf <<EOF
-filter-aaaa
-EOF
-fi
+#if [ $filter_aaaa = 0 ]; then
+#sed -i '/filter-aaaa/d' /etc/storage/dnsmasq/dnsmasq.conf
+# else 
+#sed -i '/filter-aaaa/d' /etc/storage/dnsmasq/dnsmasq.conf
+#	cat >>/etc/storage/dnsmasq/dnsmasq.conf <<EOF
+#filter-aaaa
+#EOF
+#fi
 
 
